37b271738c602b5d6b0e8b63650b1bc24432c25a
sarazhan <zhan.sara05@gmail.com>
2023-03-19 17:05:29 +0100

    file javascript

---
+++ code.js
@@ -0,0 +1,291 @@
+function posizione(id) {
+    let pedina = document.getElementById("pedina");
+    let dimensioni = pedina.getBoundingClientRect();
+    let indirizzo = document.getElementById(id);
+    let posizione = indirizzo.getBoundingClientRect();
+    pedina.style.top = (posizione.top - (dimensioni.height / 2) + (posizione.height / 2) + window.pageYOffset) + "px";
+    pedina.style.left = (posizione.left - (dimensioni.width / 2) + (posizione.width / 2) + window.pageXOffset) + "px";
+}
+
+$(document).ready(function () {
+    $("#Ellipse").click(function () {
+        posizione("Polygon_2");
+    });
+    $("#Polygon_2").click(function () {
+        posizione("Polygon_2");
+    });
+    $("#Ellipse_2").click(function () {
+        posizione("Polygon_1");
+    });
+    $("#Polygon_1").click(function () {
+        posizione("Polygon_1");
+    });
+    $("#Ellipse_3").click(function () {
+        posizione("Polygon_6");
+    });
+    $("#Polygon_6").click(function () {
+        posizione("Polygon_6");
+    });
+    $("#Ellipse_4").click(function () {
+        posizione("Polygon_5");
+    });
+    $("#Polygon_5").click(function () {
+        posizione("Polygon_5");
+    });
+    $("#Ellipse_5").click(function () {
+        posizione("Polygon_4");
+    });
+    $("#Polygon_4").click(function () {
+        posizione("Polygon_4");
+    });
+    $("#Ellipse_6").click(function () {
+        posizione("Polygon_3");
+    });
+    $("#Polygon_3").click(function () {
+        posizione("Polygon_3");
+    });
+    $("#Ellipse_7").click(function () {
+        posizione("Ellipse_7");
+    });
+    $("#Ellipse_8").click(function () {
+        posizione("Ellipse_8");
+    });
+    $("#Ellipse_9").click(function () {
+        posizione("Ellipse_9");
+    });
+    $("#Ellipse_10").click(function () {
+        posizione("Ellipse_10");
+    });
+    $("#Ellipse_11").click(function () {
+        posizione("Ellipse_11");
+    });
+    $("#Ellipse_12").click(function () {
+        posizione("Ellipse_12");
+    });
+    $("#Ellipse_13").click(function () {
+        posizione("Ellipse_13");
+    });
+    $("#Ellipse_14").click(function () {
+        posizione("Ellipse_14");
+    });
+    $("#Ellipse_15").click(function () {
+        posizione("Ellipse_15");
+    });
+    $("#Ellipse_16").click(function () {
+        posizione("Ellipse_16");
+    });
+    $("#Ellipse_17").click(function () {
+        posizione("Ellipse_17");
+    });
+    $("#Ellipse_18").click(function () {
+        posizione("Ellipse_18");
+    });
+    $("#Ellipse_19").click(function () {
+        posizione("Ellipse_19");
+    });
+    $("#Ellipse_20").click(function () {
+        posizione("Ellipse_20");
+    });
+    $("#Ellipse_21").click(function () {
+        posizione("Ellipse_21");
+    });
+    $("#Ellipse_22").click(function () {
+        posizione("Ellipse_22");
+    });
+    $("#Ellipse_23").click(function () {
+        posizione("Ellipse_23");
+    });
+    $("#Ellipse_24").click(function () {
+        posizione("Ellipse_24");
+    });
+    $("#Ellipse_25").click(function () {
+        posizione("Ellipse_25");
+    });
+    $("#Ellipse_26").click(function () {
+        posizione("Ellipse_26");
+    });
+    $("#Ellipse_27").click(function () {
+        posizione("Ellipse_27");
+    });
+    $("#Ellipse_28").click(function () {
+        posizione("Ellipse_28");
+    });
+    $("#Ellipse_29").click(function () {
+        posizione("Ellipse_29");
+    });
+    $("#Ellipse_30").click(function () {
+        posizione("Ellipse_30");
+    });
+    $("#Ellipse_31").click(function () {
+        posizione("Ellipse_31");
+    });
+    $("#Ellipse_32").click(function () {
+        posizione("Ellipse_32");
+    });
+    $("#Ellipse_33").click(function () {
+        posizione("Ellipse_33");
+    });
+    $("#Ellipse_34").click(function () {
+        posizione("Ellipse_34");
+    });
+    $("#Ellipse_35").click(function () {
+        posizione("Ellipse_35");
+    });
+    $("#Ellipse_36").click(function () {
+        posizione("Ellipse_36");
+    });
+    $("#Ellipse_37").click(function () {
+        posizione("Ellipse_37");
+    });
+    $("#Ellipse_38").click(function () {
+        posizione("Ellipse_38");
+    });
+    $("#Ellipse_39").click(function () {
+        posizione("Ellipse_39");
+    });
+    $("#Ellipse_40").click(function () {
+        posizione("Ellipse_40");
+    });
+    $("#Ellipse_41").click(function () {
+        posizione("Ellipse_41");
+    });
+    $("#Ellipse_42").click(function () {
+        posizione("Ellipse_42");
+    });
+    $("#esagono").click(function () {
+        posizione("esagono");
+    });
+    $("#centro").click(function () {
+        posizione("esagono");
+    });
+    $("#Rectangle_1_1").click(function () {
+        posizione("Rectangle_1_1");
+    });
+    $("#Rectangle_2_1").click(function () {
+        posizione("Rectangle_2_1");
+    });
+    $("#Rectangle_3_1").click(function () {
+        posizione("Rectangle_3_1");
+    });
+    $("#Rectangle_4_1").click(function () {
+        posizione("Rectangle_4_1");
+    });
+    $("#Rectangle_5_1").click(function () {
+        posizione("Rectangle_5_1");
+    });
+    $("#Rectangle_1_2").click(function () {
+        posizione("Rectangle_1_2");
+    });
+    $("#Rectangle_2_2").click(function () {
+        posizione("Rectangle_2_2");
+    });
+    $("#Rectangle_3_2").click(function () {
+        posizione("Rectangle_3_2");
+    });
+    $("#Rectangle_4_2").click(function () {
+        posizione("Rectangle_4_2");
+    });
+    $("#Rectangle_5_2").click(function () {
+        posizione("Rectangle_5_2");
+    });
+    $("#Rectangle_1_3").click(function () {
+        posizione("Rectangle_1_3");
+    });
+    $("#Rectangle_2_3").click(function () {
+        posizione("Rectangle_2_3");
+    });
+    $("#Rectangle_3_3").click(function () {
+        posizione("Rectangle_3_3");
+    });
+    $("#Rectangle_4_3").click(function () {
+        posizione("Rectangle_4_3");
+    });
+    $("#Rectangle_5_3").click(function () {
+        posizione("Rectangle_5_3");
+    });
+    $("#Rectangle_1_4").click(function () {
+        posizione("Rectangle_1_4");
+    });
+    $("#Rectangle_2_4").click(function () {
+        posizione("Rectangle_2_4");
+    });
+    $("#Rectangle_3_4").click(function () {
+        posizione("Rectangle_3_4");
+    });
+    $("#Rectangle_4_4").click(function () {
+        posizione("Rectangle_4_4");
+    });
+    $("#Rectangle_5_4").click(function () {
+        posizione("Rectangle_5_4");
+    });
+    $("#Rectangle_1_5").click(function () {
+        posizione("Rectangle_1_5");
+    });
+    $("#Rectangle_2_5").click(function () {
+        posizione("Rectangle_2_5");
+    });
+    $("#Rectangle_3_5").click(function () {
+        posizione("Rectangle_3_5");
+    });
+    $("#Rectangle_4_5").click(function () {
+        posizione("Rectangle_4_5");
+    });
+    $("#Rectangle_5_5").click(function () {
+        posizione("Rectangle_5_5");
+    });
+    $("#Rectangle_1_6").click(function () {
+        posizione("Rectangle_1_6");
+    });
+    $("#Rectangle_2_6").click(function () {
+        posizione("Rectangle_2_6");
+    });
+    $("#Rectangle_3_6").click(function () {
+        posizione("Rectangle_3_6");
+    });
+    $("#Rectangle_4_6").click(function () {
+        posizione("Rectangle_4_6");
+    });
+    $("#Rectangle_5_6").click(function () {
+        posizione("Rectangle_5_6");
+    });
+});
+
+posizione("esagono");
+let cube = document.getElementById("cube");
+let y = 1800, x = 1080;
+cube.onclick = function (){
+    let random = Math.ceil(Math.random()*6);
+    console.log(random);
+    let X, Y, Z;
+    switch (random){
+        case 1:
+            X = 0;
+            Y = 0;
+            break;
+        case 2:
+            X = 0;
+            Y = 90;
+            break;
+        case 3:
+            X = 90;
+            Y = 0;
+            break;
+        case 4:
+            X = -90;
+            Y = 0;
+            break;
+        case 5:
+            X = 0;
+            Y = -90;
+            break;
+        case 6:
+            X = 0;
+            Y = 180;
+            break;
+    }
+    x += 1800;
+    y += 1080;
+    Y = Y + y;
+    X = X + x;
+    cube.style.transform = "rotateX(" + X + "deg) " + "rotateY(" + Y + "deg)"
+}
